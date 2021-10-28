f25bc11ca3a5a858ae7adaf4980a2da3fdc38314
jshinb <jshinb@users.noreply.github.com>
2021-10-28 13:02:02 -0400

    Create explore_data.r
    initial commit

---
+++ R/explore_data.r
@@ -0,0 +1,13 @@
+############## Explore data #############################################################
+# Run this script in SciNet cluster
+#
+# Before running the script, execute the following code:
+# module load gcc/9.2.0 r/4.0.3;R
+#
+# This script is available on the GitHub repository:
+# https://github.com/jshinb/ukbb_gwas_LDL.D/tree/main/R
+#
+##########################################################################################
+
+library(tidyverse)
+library(data.table)
